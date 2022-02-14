45ecb308781366802f4a5fd35e0a7a79f7098e72
Sean <s.n.bull@live.com>
2022-02-14 18:47:10 +0000

    Collision detection

--- milestone5/bounce.py
+++ milestone5/bounce.py
@@ -4,38 +4,49 @@ except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 from vector import *
-import math
+
 
 class Wall:
-    def __init__(self, x, border, color, edge):
+    def __init__(self, x, y, width, color, face):
         self.x = x
-        self.border = border
+        self.y = y
+        self.width = width
         self.color = color
-        self.edge = edge
-        if self.edge == 'l':
+        self.face = face
+        if self.face == 'l':
             self.normal = Vector(1, 0)
-        elif self.edge == 'r':
+            self.edge = x - self.width
+        elif self.face == 'r':
+            self.edge = x + self.width
             self.normal = Vector(-1, 0)
-        elif self.edge == 'u':
+        elif self.face == 'u':
             self.normal = Vector(0, 1)
-        elif self.edge == 'd':
+            self.edge = y - self.width
+        elif self.face == 'd':
             self.normal = Vector(0, -1)
+            self.edge = y + self.width
         else:
             raise Exception("Invalid direction")
-        self.edge_r = x + self.border
 
     def draw(self, canvas):
         canvas.draw_line((self.x, 0),
                          (self.x, CANVAS_HEIGHT),
-                         self.border*2+1,
+                         self.width * 2 + 1,
                          self.color)
 
     def hit(self, ball) -> bool:
-        if self.edge == 'r':
-            h = (ball.offset_l() >= self.edge_r)
+        if self.face == 'l':
+            hit = (ball.pos.x - ball.radius <= self.edge)
+        elif self.face == "r":
+            hit = (ball.pos.x + ball.radius >= self.edge)
+        elif self.face == 'u':
+            hit = (ball.pos.y - ball.radius <= self.edge)
+        elif self.face == "d":
+            hit = (ball.pos.y + ball.radius >= self.edge)
         else:
-            h = (ball.offset_l() <= self.edge_r)
-        return h
+            raise Exception("Invalid direction")
+        return hit
+
 
 class Ball:
     def __init__(self, pos, vel, radius, border, color):
@@ -46,7 +57,7 @@ class Ball:
         self.color = color
         self.in_collision = False
 
-    def offset_l(self):
+    def offset(self):
         return self.pos.x - self.radius
 
     def update(self):
@@ -61,6 +72,7 @@ class Ball:
 
     def bounce(self, normal):
         self.vel.reflect(normal)
+
 
 class Interaction:
     def __init__(self, wall, ball):
@@ -81,17 +93,18 @@ class Interaction:
         self.ball.draw(canvas)
         self.wall.draw(canvas)
 
+
 # The canvas dimensions
 CANVAS_WIDTH = 600
 CANVAS_HEIGHT = 400
 
 # Initial position and velocity of the ball
-p = Vector(0,200)
-v = Vector(-1,0)
+p = Vector(0, 200)
+v = Vector(-1, 0)
 
 # Creating the objects
 b = Ball(p, v, 20, 20, 'blue')
-w = Wall(0, 5, 'red', 'l')
+w = Wall(0, 0, 5, 'red', 'l')
 i = Interaction(w, b)
 
 # Create a frame and assign callbacks to event handlers
