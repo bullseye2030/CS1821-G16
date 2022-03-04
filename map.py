62890c32ea60bc4fdcf9613e9b6853dd15868952
nathan <nathanmburia@gmail.com>
2022-03-04 00:07:51 +0000

    tanks map

--- map.py
+++ map.py
@@ -4,6 +4,7 @@ except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 from vector import *
+from random import randint
 
 CANVAS_HEIGHT = 600
 CANVAS_WIDTH = 600
@@ -72,18 +73,47 @@ def create_map(rows,columns): #return array of 0's to represent map
     temp = [[0 for row in range(rows+1)] for x in range(columns+1)]
     return temp
 
+
 class Map:
 
-    def __init__(self, items):
+    def __init__(self, items, rows, columns, obstacle_rate, debug = False):
         self.items = items
-
-    def generate_map(self):
-        return 1 #return complete map from array of numbers
+        self.rows = rows
+        self.columns = columns
+        self.heat_map = [[], []]    #2d array of 1's and 0's
+        self.grid = [[], []] #2d array of grid coordinates
+        self.obstacle_rate = obstacle_rate
+        self.debug = debug #if true then lines will be drawn to show the grid
 
 
     def draw(self, canvas): #callback function to draw entire map, called in update function
         for item in self.items:
             item.draw(canvas)
+
+    def build_corner(self, gamemap):
+        for y in range(self.rows):  # y because go down columns first then pick a row number
+            for x in range(self.columns):
+                if gamemap[y][x] != 0:  #check if map already has an obstacle there; if it is 1 then ignore
+                    #do checks here
+
+                    gamemap[y][x] = 1  #change gamemap after everything else is done
+
+
+    def generate_heat_map(self):    #return 2d array of numbers
+        temp_map = create_map(self.rows, self.columns)
+        for y in range(self.rows):  #y because go down columns first then pick a row number
+            for x in range(self.columns):
+                if randint(1, self.obstacle_rate) == 1:     #a 1/obstacle rate chance of creating an obstacles
+                    obstacle = randint(1, 4)
+                    if obstacle == 1:
+                        build_corner(temp_map)
+                    elif obstacle == 2:
+                        build_block(temp_map)
+                    elif obstacle == 3:
+                        build_line(temp_map)
+                    elif obstacle == 4:
+                        build_plus(temp_map)
+
 
 l = Wall(0, 0, BORDER_THICKNESS, BORDER_COLOUR, 'l')
 r = Wall(0, CANVAS_WIDTH, BORDER_THICKNESS, BORDER_COLOUR, 'r')
@@ -91,10 +121,11 @@ t = Wall(0, 0, BORDER_THICKNESS, BORDER_COLOUR, 't')
 b = Wall(0, CANVAS_HEIGHT, BORDER_THICKNESS, BORDER_COLOUR, 'b')
 ITEMS = [l, r, t, b]
 
-map = Map(ITEMS)
+
+gamemap = Map(ITEMS)
 
 frame = simplegui.create_frame("TANKS!", CANVAS_WIDTH, CANVAS_HEIGHT)
-frame.set_draw_handler(map.draw)
+frame.set_draw_handler(gamemap.draw)
 
 frame.set_canvas_background(BACKGROUND)
 
