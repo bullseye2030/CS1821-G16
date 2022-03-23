e3be510b799d8ec2b22ceb540acb81950af2430f
Sean <s.n.bull@live.com>
2022-03-23 13:39:37 +0000

    player tank and projectiles finished

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from vector import Vector
 CANVAS_WIDTH = 768  # 48*16
 CANVAS_HEIGHT = 576  # 48*12
 
+frame_number = 0
+
 items = []
 difficulty = 0
 round_number = 0
@@ -125,11 +127,11 @@ class Tank:
         self.tank_angle = 0.0  # Default angle from normal - starts tank pointing left
         self.health = health  # Health value - default range 3-0 for player tank (2 and 1 for damaged, 0 for destroyed)
         self.fire_rate = fire_rate  # Fire rate - default 1 (fires once a second) - can do draw_number % (60/fire_rate)
-        self.speed = speed  # Speed value - Vector - default 1
+        self.speed = speed  # Speed value - default 1
         self.destroyed = False
         self.sprites = self.get_sprites()
         self.dims = []
-        self.frame_number = 0
+        self.frames_til_next_fire = 0
         for sprite in self.sprites:
             self.dims.append([sprite.get_width(), sprite.get_height()])
 
@@ -153,6 +155,7 @@ class Tank:
 
     def draw(self, canvas):
         """Method to draw the tank on the canvas - tank body then cannon"""
+        global frame_number
         if not self.destroyed:
             if self.health == 3:
                 canvas.draw_image(self.sprites[0], (self.dims[0][0] / 2, self.dims[0][1] / 2),
@@ -187,10 +190,9 @@ class Tank:
                 canvas.draw_image(self.sprites[4], (self.dims[4][0] / 2, self.dims[4][1] / 2),
                                   self.dims[4], self.pos.get_p(),
                                   (self.dims[4][0], self.dims[4][1]))
-                if self.frame_number % 10 == 0:
+                if frame_number % 10 == 0:
                     self.sprites.remove(self.sprites[4])
                     self.dims.remove(self.dims[4])
-                self.frame_number += 1
             else:
                 self.destroyed = True
 
@@ -202,19 +204,18 @@ class PlayerTank(Tank):
 
     def move(self):
         """Method called on every update which checks user input and moves the tank accordingly"""
-        self.tank_angle = self.tank_angle + self.vel.angle(self.tank_angle)
         if self.keyboard.d:
-            self.vel.add(Vector(self.speed / 10, 0))
-            self.tank_angle = 270 * (math.pi / 180)
+            self.tank_angle = self.tank_angle + 0.08
+            self.cannon_angle = self.cannon_angle + 0.08
         if self.keyboard.a:
-            self.vel.add(Vector(-self.speed / 10, 0))
-            self.tank_angle = 90 * (math.pi / 180)
+            self.tank_angle = self.tank_angle - 0.08
+            self.cannon_angle = self.cannon_angle - 0.08
         if self.keyboard.w:
-            self.vel.add(Vector(0, -self.speed / 10))
-            self.tank_angle = 180 * (math.pi / 180)
+            new_vel = Vector(self.speed*1.5, 0).rotate_rad(self.tank_angle + (math.pi/2))
+            self.vel.add(new_vel)   # add vector with magnitude speed in direction tank_angle
         if self.keyboard.s:
-            self.vel.add(Vector(0, self.speed / 10))
-            self.tank_angle = 0
+            new_vel = Vector(self.speed*1.5, 0).rotate_rad(self.tank_angle + (math.pi/2) + math.pi)
+            self.vel.add(new_vel)  # add vector with magnitude speed in direction tank_angle + 180deg
         if not self.keyboard.d and not self.keyboard.a and not self.keyboard.w and not self.keyboard.s:
             self.vel = Vector(0, 0)
         if self.keyboard.q:
@@ -223,17 +224,30 @@ class PlayerTank(Tank):
             self.cannon_angle -= 0.05
 
         # set max speed
-        if self.vel.x > self.speed:
-            self.vel = Vector(min(self.vel.x, self.speed), self.vel.y)
-        if self.vel.x < -self.speed:
-            self.vel = Vector(max(self.vel.x, -self.speed), self.vel.y)
-        if self.vel.y > self.speed:
-            self.vel = Vector(self.vel.x, min(self.vel.y, self.speed))
-        if self.vel.y < -self.speed:
-            self.vel = Vector(self.vel.x, max(self.vel.y, -self.speed))
+        if self.vel.length() > self.speed:
+            self.vel = self.vel.normalize().multiply(self.speed*1.5)
+        if self.vel.length() < -self.speed:
+            self.vel = self.vel.normalize().multiply(-self.speed*1.5)
+
+    def fire(self):
+        global ITEMS
+        if self.keyboard.space:
+            if self.frames_til_next_fire == 0:
+                spawn_point = self.pos.copy()
+                spawn_point.add(Vector(self.dims[1][1]/2, 0).rotate_rad(self.cannon_angle + (math.pi/2)))
+                proj_x, proj_y = spawn_point.get_p()
+                proj_vel = Vector(3, 0).rotate_rad(self.cannon_angle + (math.pi/2))\
+                    .add(Vector(self.vel.length(), 0).rotate_rad(self.cannon_angle + (math.pi/2)))
+                proj = Projectile(proj_x, proj_y, self, proj_vel)
+                # noinspection PyTypeChecker
+                ITEMS.append(proj)
+                self.frames_til_next_fire = 120
 
     def update(self):
         self.pos.add(self.vel)
+        if self.frames_til_next_fire > 0:
+            self.frames_til_next_fire -= 1
+        self.fire()
         self.move()
 
 
@@ -253,28 +267,34 @@ class EnemyTank(Tank):
 
 
 class Projectile:
-    def __init__(self, x, y, speed):
+    def __init__(self, x, y, tank, vel):
+        self.tank = tank
         self.pos = Vector(x, y)
-        self.vel = Vector()
-        self.speed = speed
+        self.vel = vel
+        self.angle = self.tank.cannon_angle + math.pi
         self.bounces_left = 3
-        self.sprites = self.get_sprites()
-
-    def get_sprites(self):
-        """Method called by constructor to get the correct sprites, return as a list (trail image and projectile)"""
-        projectile = simplegui.load_image(
+        self.sprite = simplegui.load_image(
             "https://github.com/bullseye2030/CS1821-G16/blob/main/sprites/projectile.png?raw=true")
-        return [projectile]
-
-    def spawn(self, tank):
-        """Spawn the projectile at the given tank"""
-        pass
+        self.dims = (self.sprite.get_width(), self.sprite.get_height())
 
     def draw(self, canvas):
-        pass
+        canvas.draw_image(self.sprite,  # image,
+                          (self.dims[0] / 2, self.dims[1] / 2),  # center_source,
+                          (self.dims[0], self.dims[1]),  # width_height_source,
+                          self.pos.get_p(),  # center_dest, (where it gets drawn on the canvas)
+                          (self.dims[0], self.dims[1]),  # width_height_dest
+                          self.angle  # rotation
+                          )
+
+    def update(self):
+        self.pos.add(self.vel)
 
 
 def draw_handler(canvas):
+    global frame_number
+    frame_number += 1
+    if frame_number > 600:
+        frame_number = 1
     if menu.show or player.destroyed:
         menu.draw(canvas)
     else:
@@ -282,8 +302,8 @@ def draw_handler(canvas):
             try:
                 item.update()
                 item.draw(canvas)
-            except AttributeError:
-                pass
+            except AttributeError as e:
+                print(e)
 
 
 ITEMS = []
@@ -291,7 +311,6 @@ ITEMS = []
 kbd = Keyboard()
 menu = Menu(kbd)
 player = PlayerTank(30, 30, 3, 1, 1, kbd)
-
 
 ITEMS.append(player)
 
