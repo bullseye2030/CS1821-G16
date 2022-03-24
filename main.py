0b05ef3fa6c1ef76419652d7e4b120f23d05b2c3
Sean <s.n.bull@live.com>
2022-03-24 20:33:24 +0000

    beginning of enemy AI

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 import math
+import random
 from vector import Vector
 import map
-
 
 # Canvas Dimensions
 CANVAS_WIDTH = 768  # 48*16
@@ -33,18 +33,18 @@ class Menu:
         global difficulty, player
         if self.show:
             if not player.destroyed:
-                canvas.draw_image(self.start_image,                       # image,
-                                  (self.dims[0] / 2, self.dims[1] / 2),   # center_source,
-                                  (self.dims[0], self.dims[1]),           # width_height_source,
-                                  (self.dims[0] / 2, self.dims[1] / 2),   # center_dest,
-                                  (self.dims[0], self.dims[1])            # width_height_dest
+                canvas.draw_image(self.start_image,  # image,
+                                  (self.dims[0] / 2, self.dims[1] / 2),  # center_source,
+                                  (self.dims[0], self.dims[1]),  # width_height_source,
+                                  (self.dims[0] / 2, self.dims[1] / 2),  # center_dest,
+                                  (self.dims[0], self.dims[1])  # width_height_dest
                                   )
             else:
-                canvas.draw_image(self.end_image,                         # image,
-                                  (self.dims[0] / 2, self.dims[1] / 2),   # center_source,
-                                  (self.dims[0], self.dims[1]),           # width_height_source,
-                                  (self.dims[0] / 2, self.dims[1] / 2),   # center_dest,
-                                  (self.dims[0], self.dims[1])            # width_height_dest
+                canvas.draw_image(self.end_image,  # image,
+                                  (self.dims[0] / 2, self.dims[1] / 2),  # center_source,
+                                  (self.dims[0], self.dims[1]),  # width_height_source,
+                                  (self.dims[0] / 2, self.dims[1] / 2),  # center_dest,
+                                  (self.dims[0], self.dims[1])  # width_height_dest
                                   )
                 canvas.draw_text(str(round_number), (460, 312), 22, "white")
                 if self.keyboard.space:
@@ -127,6 +127,8 @@ class Tank:
         self.vel = Vector()
         self.cannon_angle = 0.0  # Default angle from normal - starts cannon pointing left
         self.tank_angle = 0.0  # Default angle from normal - starts tank pointing left
+        self.cannon_angle_speed = 0.05
+        self.tank_angle_speed = 0.08
         self.health = health  # Health value - default range 3-0 for player tank (2 and 1 for damaged, 0 for destroyed)
         self.fire_rate = fire_rate  # Fire rate - default 1 (fires once a second) - can do draw_number % (60/fire_rate)
         self.speed = speed  # Speed value - default 1
@@ -212,8 +214,8 @@ class Tank:
         :param obs_size:   size of obstacle
         :return: Velocity as a vector
         """
-        x_lower_lim = obs_pos.x - obs_size  #calculates the start of the hitbox region on the x axis
-        x_upper_lim = obs_pos.x + obs_size  #calculates the end of the hitbox region on the x axis
+        x_lower_lim = obs_pos.x - obs_size  # calculates the start of the hitbox region on the x axis
+        x_upper_lim = obs_pos.x + obs_size  # calculates the end of the hitbox region on the x axis
         y_lower_lim = obs_pos.y - obs_size
         y_upper_lim = obs_pos.y + obs_size
         next_pos_x = self.pos.x + self.vel.x
@@ -229,12 +231,14 @@ class Tank:
         self.vel = self.limit_pos(Vector(obstacle.obs_centre[0], obstacle.obs_centre[1]), obstacle.obs_dims[0])
 
     def update(self):
+        self.cannon_angle = self.cannon_angle % 2 * math.pi  # keep the tank angle between 0 and 2pi
+        self.tank_angle = self.tank_angle % 2 * math.pi  # keep the tank angle between 0 and 2pi
         self.pos.add(self.vel)
         self.move()
-        #code for checking collisions
-        for item in ITEMS:      #first check for collisions with tanks and projectiles
+        # code for checking collisions
+        for item in ITEMS:  # first check for collisions with tanks and projectiles
             item.check_collision(self)
-        gamemap.check_collision(self)   #then check for collisions with walls and obstacles
+        gamemap.check_collision(self)  # then check for collisions with walls and obstacles
 
 
 class PlayerTank(Tank):
@@ -245,39 +249,39 @@ class PlayerTank(Tank):
     def move(self):
         """Method called on every update which checks user input and moves the tank accordingly"""
         if self.keyboard.d:
-            self.tank_angle = self.tank_angle + 0.08
-            self.cannon_angle = self.cannon_angle + 0.08
+            self.tank_angle = self.tank_angle + self.tank_angle_speed
+            self.cannon_angle = self.cannon_angle + self.tank_angle_speed
         if self.keyboard.a:
-            self.tank_angle = self.tank_angle - 0.08
-            self.cannon_angle = self.cannon_angle - 0.08
+            self.tank_angle = self.tank_angle - self.tank_angle_speed
+            self.cannon_angle = self.cannon_angle - self.tank_angle_speed
         if self.keyboard.w:
-            new_vel = Vector(self.speed*1.5, 0).rotate_rad(self.tank_angle + (math.pi/2))
-            self.vel.add(new_vel)   # add vector with magnitude speed in direction tank_angle
+            new_vel = Vector(self.speed * 1.5, 0).rotate_rad(self.tank_angle + (math.pi / 2))
+            self.vel.add(new_vel)  # add vector with magnitude speed in direction tank_angle
         if self.keyboard.s:
-            new_vel = Vector(self.speed*1.5, 0).rotate_rad(self.tank_angle + (math.pi/2) + math.pi)
+            new_vel = Vector(self.speed * 1.5, 0).rotate_rad(self.tank_angle + (math.pi / 2) + math.pi)
             self.vel.add(new_vel)  # add vector with magnitude speed in direction tank_angle + 180deg
         if not self.keyboard.d and not self.keyboard.a and not self.keyboard.w and not self.keyboard.s:
             self.vel = Vector(0, 0)
         if self.keyboard.q:
-            self.cannon_angle += 0.05
+            self.cannon_angle += self.cannon_angle_speed
         if self.keyboard.e:
-            self.cannon_angle -= 0.05
+            self.cannon_angle -= self.cannon_angle_speed
 
         # set max speed
         if self.vel.length() > self.speed:
-            self.vel = self.vel.normalize().multiply(self.speed*1.5)
+            self.vel = self.vel.normalize().multiply(self.speed * 1.5)
         if self.vel.length() < -self.speed:
-            self.vel = self.vel.normalize().multiply(-self.speed*1.5)
+            self.vel = self.vel.normalize().multiply(-self.speed * 1.5)
 
     def fire(self):
         global ITEMS
         if self.keyboard.space:
             if self.frames_til_next_fire == 0:
                 spawn_point = self.pos.copy()
-                spawn_point.add(Vector(self.dims[1][1]/2, 0).rotate_rad(self.cannon_angle + (math.pi/2)))
+                spawn_point.add(Vector(self.dims[1][1] / 2, 0).rotate_rad(self.cannon_angle + (math.pi / 2)))
                 proj_x, proj_y = spawn_point.get_p()
-                proj_vel = Vector(3, 0).rotate_rad(self.cannon_angle + (math.pi/2))\
-                    .add(Vector(self.vel.length(), 0).rotate_rad(self.cannon_angle + (math.pi/2)))
+                proj_vel = Vector(3, 0).rotate_rad(self.cannon_angle + (math.pi / 2)) \
+                    .add(Vector(self.vel.length(), 0).rotate_rad(self.cannon_angle + (math.pi / 2)))
                 proj = Projectile(proj_x, proj_y, self, proj_vel)
                 # noinspection PyTypeChecker
                 ITEMS.append(proj)
@@ -289,18 +293,20 @@ class PlayerTank(Tank):
             self.frames_til_next_fire -= 1
         self.fire()
         self.move()
-        #code for checking collisions
-        for item in ITEMS:      #first check for collisions with tanks and projectiles
+        # code for checking collisions
+        for item in ITEMS:  # first check for collisions with tanks and projectiles
             if item == self:
                 continue
             item.check_collision(self)
-        gamemap.check_collision(self)   #then check for collisions with walls and obstacles
+        gamemap.check_collision(self)  # then check for collisions with walls and obstacles
 
 
 class EnemyTank(Tank):
     def __init__(self, x, y, health, fire_rate, speed):
         global difficulty
         super().__init__(x, y, health, fire_rate, speed)
+        self.movement = ""
+        self.frames_left_for_move = 0
         self.difficulty = difficulty
         self.sprites[0], self.sprites[2], self.sprites[3] = simplegui.load_image(
             "https://github.com/bullseye2030/CS1821-G16/blob/main/sprites/tantank.png?raw=true")
@@ -309,8 +315,71 @@ class EnemyTank(Tank):
 
     def make_move(self):
         """Method called on every update which randomly decides if and when the tank wants to move"""
-        pass
+        # Need to check for line of sight
+        # if no line of sight, move cannon left or right for x frames, and then decide next move
+        # if line of sight from cannon crosses tank, try to fire at the tank and keep tracking the tank
+        # decide if the tank also wants to move, for how many frames, and in which direction
+        line_of_sight = False
 
+        # check line of sight here
+
+        if not line_of_sight:
+            if self.movement and self.frames_left_for_move > 0:
+                if self.movement.find("rotate_cannon") != -1:
+                    if self.movement == "rotate_cannon_left":
+                        self.cannon_angle -= self.cannon_angle_speed
+                    if self.movement == "rotate_cannon_right":
+                        self.cannon_angle += self.cannon_angle_speed
+                elif self.movement.find("move_tank") != -1:
+                    if self.movement == "move_tank_right":
+                        self.tank_angle = self.tank_angle + self.tank_angle_speed
+                        self.cannon_angle = self.cannon_angle + self.tank_angle_speed
+                    if self.movement == "move_tank_left":
+                        self.tank_angle = self.tank_angle - self.tank_angle_speed
+                        self.cannon_angle = self.cannon_angle - self.tank_angle_speed
+                    if self.movement == "move_tank_forward":
+                        new_vel = Vector(self.speed * 1.5, 0).rotate_rad(self.tank_angle + (math.pi / 2))
+                        self.vel.add(new_vel)  # add vector with magnitude speed in direction tank_angle
+                    if self.movement == "move_tank_backward":
+                        new_vel = Vector(self.speed * 1.5, 0).rotate_rad(self.tank_angle + (math.pi / 2) + math.pi)
+                        self.vel.add(new_vel)  # add vector with magnitude speed in direction tank_angle + 180deg
+                    if self.frames_left_for_move == 1:  # stop on the last frame
+                        self.vel = Vector(0, 0)
+                    # set max speed
+                    if self.vel.length() > self.speed:
+                        self.vel = self.vel.normalize().multiply(self.speed * 1.5)
+                    if self.vel.length() < -self.speed:
+                        self.vel = self.vel.normalize().multiply(-self.speed * 1.5)
+                elif self.movement == "do_nothing":
+                    pass
+                self.frames_left_for_move -= 1
+            else:
+                self.movement = ""
+            if not self.movement:
+                choice = random.randint(0, 12)  # 50% of the time - nothing happens for a x frames
+                self.movement = {
+                    0: "rotate_cannon_left",
+                    1: "rotate_cannon_right",
+                    2: "move_tank_right",
+                    3: "move_tank_left",
+                    4: "move_tank_forward",
+                    5: "move_tank_backward",
+                    **dict.fromkeys([6, 7, 8, 9, 10, 11], "do_nothing")  # 6 to 11 are "do_nothing"
+                }.get(choice)
+                self.frames_left_for_move = random.randint(30, 300)  # all moves run for 0.5 - 5 seconds
+        else:
+            # Stop movements
+            self.frames_left_for_move = 0
+            self.movement = ""
+            # See if cannon is pointing at player (if cannon angle is correct)
+            # if not:
+            #   Move cannon towards player
+            # else:
+            #   if enemy can fire:
+            #       Fire at player
+
+    def update(self):
+        self.make_move()
 
 class Projectile:
     def __init__(self, x, y, tank, vel):
@@ -339,16 +408,15 @@ class Projectile:
             self.vel = Vector(0, 0)
             print(self.vel.angle(normal))
 
-
     def hit(self, tank):  # if hitting a tank then damage it
         tank.damage()
 
     def update(self):
         self.pos.add(self.vel)
-        #code for checking collisions
-        for item in ITEMS:      #first check for collisions with tanks and projectiles
+        # code for checking collisions
+        for item in ITEMS:  # first check for collisions with tanks and projectiles
             item.check_collision(self)
-        gamemap.check_collision(self)   #then check for collisions with walls and obstacles
+        gamemap.check_collision(self)  # then check for collisions with walls and obstacles
 
 
 def draw_handler(canvas):
@@ -368,15 +436,27 @@ def draw_handler(canvas):
                 print(e)
 
 
+def new_round():
+    global round_number, difficulty
+    enemies = []
+    gamemap = map.create_gamemap()
+    no_tanks_to_spawn = min(max(1, (round_number // 2)), 8)
+    for i in range(no_tanks_to_spawn):
+        # find a location that is valid to spawn
+        x, y = 0, 0
+        enemies.append(EnemyTank(x, y, 1, 1, 1))
+    return gamemap, enemies
+
+
 ITEMS = []
 
 kbd = Keyboard()
-gamemap = map.create_gamemap()
+gamemap, enemies = new_round()
 menu = Menu(kbd)
 player = PlayerTank(30, 30, 3, 1, 1, kbd)
-
+for enemy in enemies:
+    ITEMS.append(enemy)
 ITEMS.append(player)
-
 
 frame = simplegui.create_frame("Tanks", CANVAS_WIDTH, CANVAS_HEIGHT)
 frame.set_draw_handler(draw_handler)
